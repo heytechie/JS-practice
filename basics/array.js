bc928de2f01fa3ad9893d8be8fa5b04a94b543c7
heytechie <tanishk.23bai10933@vitbhopal.ac.in>
2025-04-19 00:44:53 +0530

    19 april

--- basics/array.js
+++ basics/array.js
@@ -1,13 +1,13 @@
 //to merge arrays
-const arr1 = ["iron man","Spiderman","thor"]
-const arr2 = ["batman","superman","joker"]
+const arr1 = ["iron man", "Spiderman", "thor"];
+const arr2 = ["batman", "superman", "joker"];
 //using concat
 // const newarr = arr1.concat(arr2)
 // console.log(newarr)
-//using spread operator 
+//using spread operator
 // const newarr = [...arr1,...arr2];
 // console.log(newarr)
 //using flat cmd to flat an array and returns a normal array for an array of array
-const another_arr = [1,2,3,[4,5,6,[7,8,9]]];
-const realAnother_arr = another_arr.flat(Infinity)
-console.log(realAnother_arr)
+const another_arr = [1, 2, 3, [4, 5, 6, [7, 8, 9]]];
+const realAnother_arr = another_arr.flat(Infinity);
+console.log(realAnother_arr);
--- basics/objects.js
+++ basics/objects.js
@@ -1,21 +1,58 @@
-// creating object 
+// creating object
 // const symnew = symbol(key1)
-const newobj = {
-    name : "Tanishk",
-    age : 20,
-    location : "Amravati",
-    // [symnew] : "two"
-}
+// const newobj = {
+//   name: "Tanishk",
+//   age: 20,
+//   location: "Amravati",
+//   // [symnew] : "two"
+// };
 // how we access object
 //1
-console.log(newobj.name);
+// console.log(newobj.name);
 //but this wont work in various things like symbols
 // console.log(newobj.symnew);
-//2 
-console.log(newobj["name"]);
+//2
+// console.log(newobj["name"]);
 // console.log(newobj[symnew]);
-console.log(newobj);
+// console.log(newobj);
+// creating objects inside objects
+const newobj = {
+  name: "Tanishk",
+  age: 20,
+  location: "Amravati",
+  // [symnew] : "two"
+  address: {
+    city: "Amravati",
+    state: "Maharashtra",
+    country: "India",
+  },
+};
+//accessing objects inside objects
+// console.log(newobj.address.city);
+// joining objects
+const newobj2 = {
+  1: "a",
+  2: "b",
+  3: "c",
+};
 
-
-
-
+const newobj3 = {
+  4: "d",
+  5: "e",
+  6: "f",
+};
+// using assign method
+// const newobj4 = Object.assign({}, newobj2, newobj3);
+// console.log(newobj4);
+//using spread operator(prefered)
+// const newobj5 = { ...newobj2, ...newobj3 };
+// console.log(newobj5);
+// extracting objects keys(array of keys)
+// const keys = Object.keys(newobj2);
+// console.log(keys);
+// extracting objects values(array of values)
+// const values = Object.values(newobj2);
+// console.log(values);
+// extracting objects entries(array of entries)
+// const entries = Object.entries(newobj2);
+// console.log(entries);
